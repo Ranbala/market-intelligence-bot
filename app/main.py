27eb16accfdd5085f9607017e76d44c3e3517150
Balakumar Moorthy <balakumarmoorthy@Balakumars-MacBook-Pro.local>
2026-05-23 22:33:31 +0530

    Improved NSE filing intelligence and database initialization

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ from services.llm_router import (
 from dotenv import load_dotenv
 from utils.date_extractor import extract_filing_dates
 from utils.filing_printer import print_filing_event
-from database import create_tables
+from database import init_db
+
 
 load_dotenv()
 
@@ -176,7 +177,7 @@ def format_filing_telegram_message(
 def main():
 
     init_db()
-    create_tables()
+
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     debug_file = open(
